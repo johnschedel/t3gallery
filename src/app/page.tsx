3eac6dedcd4e0df3721c0bd236d1eb0a5be85158
John Schedel <john.r.schedel@gmail.com>
2024-05-12 21:38:59 -0400

    prettier + fileurls

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://utfs.io/f/08baa2ab-fb2a-4ea4-ad1b-9c87ba2afd80-730tk2.png",
+  "https://utfs.io/f/b7f83cca-fcc0-43b7-8d98-7d19836e7f9a-dz5gws.png",
+  "https://utfs.io/f/aa296371-c52c-431e-8a7c-42b005f04f36-dz5gwq.png",
+  "https://utfs.io/f/8cc13f2c-a36c-4e78-b569-df95fbfaafba-dz5gvw.png",
+];
+
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
