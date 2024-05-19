c038ea9b3093c4ecb4ca5b5edefbe8cf1ecfc134
John Schedel <john.r.schedel@gmail.com>
2024-05-18 21:41:56 -0400

    database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,13 +14,18 @@ const mockImages = mockUrls.map((url, index) => ({
 }));
 
 export default async function HomePage() {
-  const posts = db.query.posts.findMany();
+  const posts = await db.query.posts.findMany();
+
+  console.log(posts);
 
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
-          <div key={image.id} className="w-48">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
+        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+          <div key={image.id + "-" + index} className="w-48">
             <img src={image.url} />
           </div>
         ))}
