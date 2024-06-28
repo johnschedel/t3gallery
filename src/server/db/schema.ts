b0ab14a5c7409ec6605ee263ba2e3fd6fdda397b
John Schedel <john.r.schedel@gmail.com>
2024-06-28 15:41:15 -0400

    schema images table

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -19,7 +19,7 @@ import {
 export const createTable = pgTableCreator((name) => `t3gallery_${name}`);
 
 export const images = createTable(
-  "post",
+  "image",
   {
     id: serial("id").primaryKey(),
     name: varchar("name", { length: 256 }).notNull(),
